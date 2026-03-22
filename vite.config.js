f7e7b8e47ca677135187c7d143fcacfa292eb789
Prabhu <desirablecoder@gmail.com>
2026-03-22 12:36:54 +0530

    vite pathc fix for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./desirablecoder-portfolio",
+  base: process.env.VITE_BASE_PATH || "./desirablecoder-portfolio",
   plugins: [react(), tailwindcss()],
   build: {
     minify: false,
